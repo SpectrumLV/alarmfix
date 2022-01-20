104acbe36a49f49fa17ee19ec8387e16913bb728
Dima <dmitrijs.sizovs@gmail.com>
2022-01-20 18:02:05 +0200

    lol

--- app/src/main/java/com/learntodroid/simplealarmclock/mat_activity.java
+++ app/src/main/java/com/learntodroid/simplealarmclock/mat_activity.java
@@ -10,5 +10,6 @@ public class mat_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mat);
+        ///jviwenviw
     }
 }
