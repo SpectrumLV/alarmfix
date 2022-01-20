e49ac29f913be8d015ffd8c4bf26d30c84364cf7
Reinis <reinisis.br@gmail.com>
2022-01-20 17:47:07 +0200

    pogu izveidošana

---
+++ app/src/main/java/com/learntodroid/simplealarmclock/mat_activity.java
@@ -0,0 +1,14 @@
+package com.learntodroid.simplealarmclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mat_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mat);
+    }
+}
