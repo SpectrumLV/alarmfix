55d5a0bdebf3afbdf93d6d261abf4335460943a4
Reinis <reinisis.br@gmail.com>
2022-03-19 20:00:09 +0200

    share strada

--- app/src/main/java/com/learntodroid/simplealarmclock/ActivitySuccess.java
+++ app/src/main/java/com/learntodroid/simplealarmclock/ActivitySuccess.java
@@ -1,22 +1,46 @@
 package com.learntodroid.simplealarmclock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.learntodroid.simplealarmclock.activities.RingActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class ActivitySuccess extends AppCompatActivity {
     private ImageButton button;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+        button = (ImageButton) findViewById(R.id.btn_share);
+        imageView = findViewById(R.id.quote);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Now share image function will be called
+                // here we  will be passing the text to share
+                // Getting drawable value from image
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                shareImageandText(bitmap);
+            }
+        });
 
         button = (ImageButton) findViewById(R.id.btn_more);
         button.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +67,42 @@ public class ActivitySuccess extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void shareImageandText(Bitmap bitmap) {
+        Uri uri = getmageToShare(bitmap);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        // putting uri of image to be shared
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // adding text to share
+        intent.putExtra(Intent.EXTRA_TEXT, "Quote by AlarMe #alarme");
+
+        // Add subject Here
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+
+        // setting type to image
+        intent.setType("image/jpeg");
+
+        // calling startactivity() to share
+        startActivity(Intent.createChooser(intent, "Share Via"));
+    }
+
+    private Uri getmageToShare(Bitmap bitmap) {
+        File imagefolder = new File(getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imagefolder.mkdirs();
+            File file = new File(imagefolder, "quote.jpeg");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            uri = FileProvider.getUriForFile(this, "com.learntodroid.simplealarmclock", file);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return uri;
     }
 }
