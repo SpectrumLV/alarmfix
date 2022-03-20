fd91a8f66f9e0f1d2fd3a404d5daadba43495715
Reinis <reinisis.br@gmail.com>
2022-03-20 15:45:13 +0200

    quiz activity bez bildes

--- app/src/main/java/com/learntodroid/simplealarmclock/activity_quiz.java
+++ app/src/main/java/com/learntodroid/simplealarmclock/activity_quiz.java
@@ -1,14 +1,141 @@
 package com.learntodroid.simplealarmclock;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.lang.reflect.AnnotatedElement;
+import java.util.Random;
 
 public class activity_quiz extends AppCompatActivity {
+    Button answer1, answer2, answer3, answer4;
+    ProgressBar prog_timer;
+    TextView tv_timer, score, question;
+
+    int secondsRemaining = 30;
+
+    private  Questions mQuestions = new Questions();
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQestonsLength = mQuestions.mQuestions.length;
+
+    Random r;
+
+
+    CountDownTimer timer = new CountDownTimer(30000,1000) {
+        @Override
+        public void onTick(long l) {
+            secondsRemaining--;
+            prog_timer.setProgress(30-secondsRemaining);
+            tv_timer.setText(Integer.toString(secondsRemaining) + "sec");
+
+            if (mScore == 3){
+                Intent intent = new Intent(activity_quiz.this, ActivitySuccess.class);
+                startActivity(intent);
+                finish();
+                timer.cancel();
+            }
+
+
+        }
+
+
+        @Override
+        public void onFinish() {
+
+            Intent intent = new Intent(activity_quiz.this, FailedTask.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        r = new Random();
+
+        answer1 = (Button) findViewById(R.id.answer1);
+        answer2 = (Button) findViewById(R.id.answer2);
+        answer3 = (Button) findViewById(R.id.answer3);
+        answer4 = (Button) findViewById(R.id.answer4);
+
+        score =  findViewById(R.id.score);
+        question =  findViewById(R.id.question);
+        tv_timer = findViewById(R.id.tv_timer);
+        prog_timer = findViewById(R.id.progressBar);
+
+        secondsRemaining = 30;
+        timer.start();
+
+        score.setText("Score: " + mScore);
+
+        updateQuestoin(r.nextInt(mQestonsLength));
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (answer1.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestoin(r.nextInt(mQestonsLength));
+                }
+            }
+        });
+
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (answer2.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestoin(r.nextInt(mQestonsLength));
+                }
+            }
+        });
+
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (answer3.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestoin(r.nextInt(mQestonsLength));
+                }
+            }
+        });
+
+        answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (answer4.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestoin(r.nextInt(mQestonsLength));
+                }
+            }
+        });
+
     }
+
+    private void updateQuestoin(int num) {
+        question.setText(mQuestions.getQuestions(num));
+        answer1.setText(mQuestions.getChoice1(num));
+        answer2.setText(mQuestions.getChoice2(num));
+        answer3.setText(mQuestions.getChoice3(num));
+        answer4.setText(mQuestions.getChoice4(num));
+
+        mAnswer = mQuestions.getCorrectAnswer(num);
+    }
+
+
 }
